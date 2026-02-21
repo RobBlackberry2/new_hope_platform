f54f3620eeb1bef620f31d3f1803f227b044be0b
Sebas090909 <133933733+Sebastian14140909@users.noreply.github.com>
2026-02-21 11:44:38 -0600

    Centrado y actualización de formato
    Se centraron todas las tablas para cumplir con la estética visual de la plataforma

--- app/config/config.php
+++ app/config/config.php
@@ -2,9 +2,9 @@
 // Deben ajustar los valores segun su entorno local
 return [
     'db' => [
-        'host' => 'localhost',
+        'host' => '127.0.0.1',
         'user' => 'root',
-        'pass' => 'Riolu21.',
+        'pass' => '',
         'name' => 'new_hope_platform',
         'port' => 3306,
         'charset' => 'utf8mb4',
